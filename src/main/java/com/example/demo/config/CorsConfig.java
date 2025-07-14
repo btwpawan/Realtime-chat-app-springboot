ca282d038b465f7cbd3bb2a7bd4ce2e5e4291196
Pawan Sanap <148002630+btwpawan@users.noreply.github.com>
2025-07-14 13:30:47 +0530

    Create CorsConfig.java

---
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,15 @@
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
