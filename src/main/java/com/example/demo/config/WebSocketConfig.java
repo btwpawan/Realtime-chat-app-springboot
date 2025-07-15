e6798f2ffaca4fc7cbdcccbb2abc605de9a5694c
Pawan Sanap <148002630+btwpawan@users.noreply.github.com>
2025-07-15 11:53:14 +0530

    Update WebSocketConfig.java

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -12,8 +12,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        .setAllowedOriginPatterns("*")
+        .withSockJS();
+
     }
 
     @Override
